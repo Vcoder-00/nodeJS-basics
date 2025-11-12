8818f064bbc4e86661c4bb1f965f4cd3f3974158
Vitor Lopes <vcoder00@gmail.com>
2025-11-11 23:53:57 -0300

    feat: implementando CRUD com express

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 // const { Person } = require("./person");
 
 const dotenv = require('dotenv');
+
 dotenv.config();
+
 const connectToDB = require("./src/database/connect"); //'import do node.js'
 
 connectToDB();
 
+
 // require("./modules/path");
 // require("./modules/fs");
-// require("./modules/http"); 
-// require('./modules/express');
+// require("./modules/http");
+
+require('./modules/express');
 
 
 // const person = new Person("Hello World");
--- modules/express.js
+++ modules/express.js
@@ -1,25 +1,78 @@
 const express = require('express');
-
+const UserModel = require('../src/models/user.model');
 const app = express();
+
+app.use(express.json())// express vai reconhecer que as requisições sempre serão feitas em JSON
 
 const port = 8080;
 
 app.get('/home', (req, res) => {
     res.contentType("text/html");
-    res.status(200).send("<h1>Hello World</h1>");       
+    res.status(200).send("<h1>Hello World</h1>");
 });
 
-app.get("/users", (req, res) => {
-    const users = [{
-        name: "John Doe",
-        email: "john@doe.com",
-    },
-    {
-        name: "Anne Doe",
-        email: "anne@doe.com",
+app.get("/users", async (req, res) => { // listar usuarios
+    try {
+        const users = await UserModel.find({});
+
+        res.status(200).json(users);
     }
-    ];
+    catch (error) {
+        return res.status(500).send(error.message);
+    }
     res.status(200).json(users);
 })
+
+app.get("/users/:id", async (req, res) => {// Read um usuario com base no id
+    try {
+        const id = req.params.id;
+        const user = await UserModel.findById(id);
+
+        res.status(200).json(user);
+    }
+    catch (error) {
+        return res.status(500).send(error.message);
+    }
+    res.status(200).json(user);
+});
+
+app.post('/users', async (req, res) => { //create do novo usuario
+    try {
+        const user = await UserModel.create(req.body); // create é promise por isso uso do async e try/catch
+
+        res.status(201).json(user);
+    }
+    catch (error) {
+        if (error.name === 'ValidationError') {
+            return res.status(400).json({ error: error.message }); //erro de validação
+        }
+        res.status(500).send(error.message);// outros erros
+    }
+});
+
+app.patch('/users/:id', async (req, res) => { // Update de uma variavel do usuario
+    try {
+        const id = req.params.id;
+        const user = await UserModel.findByIdAndUpdate(id, req.body, {new: true});
+
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+})
+
+app.delete('/users/:id', async (req, res) => { //delete do novo usuario
+    try {
+        const user = await UserModel.findByIdAndDelete(req.params.id); 
+
+        res.status(200).json(user);
+    }
+    catch (error) {
+        if (error.name === 'ValidationError') {
+            return res.status(400).json({ error: error.message }); //erro de validação
+        }
+        res.status(500).send(error.message);// outros erros
+    }
+});
 
 app.listen(port, () => console.log(`Rodando express na porta ${port}!`));
